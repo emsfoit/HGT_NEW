7285d6423f3049da62c33b3b2ae8407eb411d264
Moe <emsfoit@gmail.com>
2021-08-04 15:33:29 +0200

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -23,7 +23,7 @@ def load_filter_by_time(file_path, sep="\t", max_size=10, time_key=None, chunksi
       result = pd.concat([result, df])
     return result
 
-def merge_csvfiles(f1=None, f2=None, sep="\t", max_size=10, merge_on=""):
+def merge_csvfiles(f1=None, f2=None, sep="\t", merge_on=""):
   df1 = pd.read_csv(f1, sep=sep)
   df2 = pd.read_csv(f2, sep=sep)
   df = df1.merge(df2, how="inner", on=merge_on)
@@ -32,21 +32,22 @@ def merge_csvfiles(f1=None, f2=None, sep="\t", max_size=10, merge_on=""):
 def filter_csv_by_att_from_another_csv(f1, f2, sep="\t", f2_on="", f1_on=""):
   df1 = pd.read_csv(f1, sep=sep)
   df2 = pd.read_csv(f2, sep=sep)
-  common = df1.merge(df2, right_on=f2_on, left_on=f1_on)
-  result = df1[df1[f1_on].isin(common[f1_on])]
+  common = df1.merge(df2, right_on=f1_on, left_on=f2_on)
+  result = df1[df1[f2_on].isin(common[f2_on])]
   return result
 
 class OAG_SAMPLES:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+  def __init__(self):
+    # Take as params
     self.main_file_path = ""
-  
+    self.time = [{'max_time': 2015}, {'min_time': 2014, 'max_time': 2017}, {'min_time': 2016}]
+    self.max_size = 10000
+
   def run(self):
     self.filter_main_file()
     self.filter_pa_file()
     self.filter_pf_file()
-    self.filter_pf_file()
+    self.filter_fh_file()
     self.filter_pr_file()
     self.filter_PAuAf_file()
 
@@ -54,7 +55,7 @@ class OAG_SAMPLES:
   def filter_main_file(self):
     file_name = "Papers_CS_20190919.tsv"
     main_file_path = input_dir + file_name
-    current_full_data = load_filter_by_time(file_path=main_file_path, time_key="PublishYear", time=[{'max_time': 2015}, {'min_time': 2014, 'max_time': 2017}, {'min_time': 2016}])
+    current_full_data = load_filter_by_time(file_path=main_file_path, time_key="PublishYear", time=self.time, max_size=self.max_size)
     current_full_data.to_csv(output_dir + file_name, index=False, sep="\t")
     self.main_file_path = output_dir + file_name
 
@@ -72,19 +73,19 @@ class OAG_SAMPLES:
     data = merge_csvfiles(f1=main_file_path, f2=file_path, merge_on="PaperId")
     data.loc[:, ['PaperId', 'FieldOfStudyId']].to_csv(output_dir + file_name, index=False, sep="\t")
 
-  def filter_pf_file(self):
+  def filter_fh_file(self):
     # FHierarchy_20190919.tsv
     # ['ChildFosId',	'ParentFosId',	'ChildLevel',	'ParentLevel']
     # check if (ChildFosId in ffl) and (ParentFosId in ffl):
     # Read ['PaperId', 'FieldOfStudyId'] from PF_CS_20190919 and save it in ffl
     ffl = {}
-    file_name = "PF_CS_20190919.tsv"
-    df = pd.read_csv(output_dir + file_name, sep="\t")
+    file_name1 = "PF_CS_20190919.tsv"
+    df = pd.read_csv(output_dir + file_name1, sep="\t")
     for ind in df.index:
       ffl[df['FieldOfStudyId'][ind]] = True
     file_name = "FHierarchy_20190919.tsv"
     file_path = input_dir + file_name
-    df = pd.read_csv(output_dir + file_name, sep="\t")
+    df = pd.read_csv(file_path, sep="\t")
     df = df[(df["ChildFosId"].isin(ffl) & df["ParentFosId"].isin(ffl))]
     df.to_csv(output_dir + file_name, index=False, sep="\t")
 
@@ -103,10 +104,6 @@ class OAG_SAMPLES:
     data = filter_csv_by_att_from_another_csv(f1=file_path, f2=main_file_path, sep="\t", f1_on="PaperId", f2_on="PaperSeqid")
     data.to_csv(output_dir + file_name, index=False, sep="\t")
 
-
-
-# filter_main_file()
-# filter_pa_file()
 OAG_SAMPLES().run()
 
 
